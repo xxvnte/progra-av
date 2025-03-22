2620d15b0375ac55623a01b40c1ef2dbc3d31283
Vicente Leiva <vicente.leiva@smartside.com>
2025-03-21 23:41:40 -0300

    update desafio-ayudantia1

--- ay1/desafio-ayudantia1.cpp
+++ ay1/desafio-ayudantia1.cpp
@@ -50,75 +50,69 @@ public:
         }
     }
     // desafio
-    void BattleRoyale(int cantidadGuerreros, Guerrero gList[100])
+    void BattleRoyale(int tam, Guerrero gList[])
     {
         cout << "Comienza la batalla campal!" << endl;
 
-        int tam = cantidadGuerreros;
+        int ronda = 0; // variable solo para mostrar el numero de ronda, no es necesario
 
         while (tam > 1)
         {
+            bool aux = true;
             for (int i = 0; i < tam - 1; i++)
             {
-                if (gList[i].getNivelDePelea() == 0)
-                    continue;
-
-                if (gList[i].nivelDePelea > gList[i + 1].nivelDePelea)
+                if (gList[i].getNivelDePelea() != gList[i + 1].getNivelDePelea())
                 {
-                    cout << gList[i].nombre << " ha ganado el round " << i + 1 << endl;
-                    gList[i + 1].setNivelDePelea(0);
-                }
-                else if (gList[i].nivelDePelea < gList[i + 1].nivelDePelea)
-                {
-                    cout << gList[i + 1].nombre << " ha ganado el round " << i + 1 << endl;
-                    gList[i].setNivelDePelea(0);
-                }
-                else
-                {
-                    cout << "Ha sido un empate!" << endl;
-                }
-            }
-
-            tam = 0;
-            for (int i = 0; i < cantidadGuerreros; i++)
-            {
-                if (gList[i].getNivelDePelea() != 0)
-                {
-                    tam++;
-                }
-            }
-
-            for (int i = 0; i < cantidadGuerreros; i++)
-            {
-                if (gList[i].getNivelDePelea() != 0)
-                {
-                    cout << gList[i].getNombre() << " es el ganador final!" << endl;
+                    aux = false;
                     break;
                 }
             }
+
+            if (aux)
+            {
+                cout << "Todos los guerreros tienen el mismo nivel de pelea, no se puede continuar con la batalla" << endl;
+                return;
+            }
+
+            for (int i = 0; i < tam - 1; i++)
+            {
+                cout << "ronda " << ronda + 1 << ": " << gList[i].getNombre() << " vs " << gList[i + 1].getNombre() << endl;
+
+                if (gList[i].getNivelDePelea() > gList[i + 1].getNivelDePelea())
+                {
+                    cout << gList[i].getNombre() << " ha ganado el round " << ronda + 1 << endl;
+                    gList[i + 1] = gList[tam - 1];
+                    tam--;
+                    ronda++;
+                }
+                else if (gList[i].getNivelDePelea() < gList[i + 1].getNivelDePelea())
+                {
+                    cout << gList[i + 1].getNombre() << " ha ganado el round " << ronda + 1 << endl;
+                    gList[i] = gList[tam - 1];
+                    tam--;
+                    ronda++;
+                }
+                else
+                {
+                    cout << "el round " << ronda + 1 << " ha sido un empate!" << endl;
+                    ronda++;
+                }
+            }
         }
+        cout << gList[0].getNombre() << " es el ganador final!" << endl;
     }
 };
 
 int main()
 {
     Guerrero pepito("pepito", 100);
-    Guerrero juanito("juanito", 80);
+    Guerrero juanito("juanito", 500);
+    Guerrero pedrito("pedrito", 900);
+    Guerrero pablito("pablito", 500);
 
-    cout << pepito.fight(juanito) << endl; // pepito vs juanito
+    Guerrero guerreros[4] = {pablito, juanito, pedrito, pepito};
 
-    juanito.setNivelDePelea(120);
-    cout << juanito.getNombre() << " ahora tiene un nivel de pelea de: " << juanito.getNivelDePelea() << endl;
-
-    cout << juanito.fight(pepito) << endl; // revancha
-
-    // desafio
-
-    Guerrero pedrito("pedrito", 200);
-
-    Guerrero guerreros[3] = {pepito, juanito, pedrito};
-
-    pepito.BattleRoyale(3, guerreros);
+    pepito.BattleRoyale(4, guerreros);
 
     return 0;
 }
